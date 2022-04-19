213cba36bc489d3407c4a0e23e18b6247e149dc7
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-04-19 12:24:11 -0700

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Falcon Template
+
+A template for use with 
+[cargo-generate](https://github.com/cargo-generate/cargo-generate)
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use libfalcon::{cli::run, error::Error, Runner, unit::gb};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    let mut d = Runner::new("duo");
+
+    // nodes
+    let violin = d.node("violin", "helios-1.1", 4, gb(4));
+    let piano = d.node("piano", "helios-1.1", 4, gb(4));
+
+    // links
+    d.link(violin, piano);
+
+    run(&mut d).await?;
+
+    Ok(())
+}
