a0dd58c43afc62d1d7494399aff1c5bf47f63406
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-06-07 07:36:39 -0700

    plumb project name

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use libfalcon::{cli::run, error::Error, Runner, unit::gb};
 #[tokio::main]
 async fn main() -> Result<(), Error> {
 
-    let mut d = Runner::new("duo");
+    let mut d = Runner::new("{{project-name}}");
 
     // nodes
     let violin = d.node("violin", "helios-1.1", 4, gb(4));
