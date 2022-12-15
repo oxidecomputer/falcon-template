f0db46952e2b6014863162f824cec460299c2220
Rain <rain@oxide.computer>
2022-12-14 18:15:47 -0800

    Update example to point to helios-1.3

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ async fn main() -> Result<(), Error> {
     let mut d = Runner::new("{{project-name}}");
 
     // nodes
-    let violin = d.node("violin", "helios-1.1", 4, gb(4));
-    let piano = d.node("piano", "helios-1.1", 4, gb(4));
+    let violin = d.node("violin", "helios-1.3", 4, gb(4));
+    let piano = d.node("piano", "helios-1.3", 4, gb(4));
 
     // links
     d.link(violin, piano);
