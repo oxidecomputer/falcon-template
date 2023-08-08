873af5199b6df6e2eb3ed28eb35f060fd43cb7b0
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2023-08-07 17:25:29 -0700

    bump to helios 2

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ async fn main() -> Result<(), Error> {
     let mut d = Runner::new("{{crate_name}}");
 
     // nodes
-    let violin = d.node("violin", "helios-1.3", 4, gb(4));
-    let piano = d.node("piano", "helios-1.3", 4, gb(4));
+    let violin = d.node("violin", "helios-2.0", 4, gb(4));
+    let piano = d.node("piano", "helios-2.0", 4, gb(4));
 
     // links
     d.link(violin, piano);
